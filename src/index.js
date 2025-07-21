5e6eb61db1833403fd3b850516dd76bd86cd9670
mhamad raad <hamaraad883@gmail.com>
2025-07-21 17:12:17 +0300

    add react router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Cashier from './pages/Cashier';
+import Warehouse from './pages/Warehouse';
+import Admin from './pages/Admin';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter([
+  { path: '/', Component: Cashier },
+  { path: '/warehouse', Component: Warehouse },
+  { path: '/admin', Component: Admin },
+  { path: '*', Component: () => <h1>404 Not Found</h1> },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/Warehouse.jsx
@@ -0,0 +1,9 @@
+const WareHouse = () => {
+  return (
+    <div>
+      <h1>WareHouse Page</h1>
+    </div>
+  );
+};
+
+export default WareHouse;
